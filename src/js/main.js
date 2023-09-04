a2d3be8c5156b938dc2579b1b0d6576405bc7917
Hakan Bilgin <hbi99@hotmail.com>
2023-09-04 10:52:44 +0200

    base functionality

--- src/js/main.js
+++ src/js/main.js
@@ -7,21 +7,135 @@
 @import "./classes/shader.js"
 @import "./modules/vecTypes.js"
 
+@import "./modules/test.js"
+
 
 const yshader = {
 	init() {
 		// fast references
-		this.content = window.find("content");
+		this.els = {
+			content: window.find("content"),
+			canvas: window.find(".main-canvas"),
+			details: {
+				resolution: window.find(".resolution"),
+				quality: window.find(".quality"),
+				time: window.find(".time"),
+				frameRate: window.find(".frameRate"),
+				state: window.find(".state"),
+			},
+			textures: {
+				el: window.find(".textures"),
+				iChannel0: window.find(".iChannel0")[0],
+				iChannel1: window.find(".iChannel1")[0],
+				iChannel2: window.find(".iChannel2")[0],
+				iChannel3: window.find(".iChannel3")[0],
+			}
+		};
+		// check debug mode
+		if (this.debug) this.els.content.addClass("debug");
+
+		// DEV-ONLY-START
+		Test.init(this);
+		// DEV-ONLY-END
 	},
 	dispatch(event) {
+		let Self = yshader,
+			name,
+			value,
+			el;
 		switch (event.type) {
+			// system events
 			case "window.init":
+				// populate menu
+				window.bluePrint
+					.selectNodes(`//Shaders/*[not(@hidden)]`)
+					.map(xShader => window.menuBar.add({
+						"parent": "//MenuBar//Menu[@name='Shaders']",
+						"name": xShader.getAttribute("name"),
+						"click": "select-shader",
+						"check-group": "selected-file",
+						// "is-checked": 1,
+					}));
 				break;
+			// custom events
 			case "open-help":
 				karaqu.shell("fs -u '~/help/index.md'");
 				break;
+			case "set-quality":
+				Self.quality(+event.arg);
+				break;
+			case "select-shader":
+				name = event.name || event.xMenu.getAttribute("name");
+				Self.start(name);
+				break;
+			case "toggle-shader":
+			case "pause-shader":
+				Self.pause();
+				break;
 		}
+	},
+	pause() {
+		if (this.shader) this.shader.pauseTime();
+		// make sure window is focused
+		// if (!window.isFocues) window.focus();
+	},
+	quality(i) {
+		this.shader.quality = +i;
+		// make sure window is focused
+		// if (!window.isFocues) window.focus();
+	},
+	start(name) {
+		let xPath = `//Shaders/*[@name="${name}"]`,
+			xShader = window.bluePrint.selectSingleNode(xPath),
+			xRenderpass = xShader.selectNodes(`./renderpass`),
+			data = {
+				ver: "0.1",
+				renderpass: []
+			};
+		// loop render passes
+		xRenderpass.map(xPass => {
+			let pass = {
+					inputs: [],
+					outputs: [],
+					code: xPass.selectSingleNode(`./code`).textContent,
+					type: xPass.getAttribute("type"),
+				};
+			// loop input nodes
+			xPass.selectNodes(`./inputs`).map(xInput => {
+				pass.inputs.push({
+						id: xInput.getAttribute("id"),
+						channel: xInput.getAttribute("channel"),
+						filepath: xInput.getAttribute("filepath"),
+						type: xInput.getAttribute("type"),
+						sampler: {
+						    filter: xInput.getAttribute("s-filter"),
+						    wrap: xInput.getAttribute("s-wrap"),
+						    vflip: xInput.getAttribute("s-vflip"),
+						    srgb: xInput.getAttribute("s-srgb"),
+						    internal: xInput.getAttribute("s-internal"),
+						}
+					});
+			});
+			xPass.selectNodes(`./outputs`).map(xOutput => {
+				pass.outputs.push({
+						id: xOutput.getAttribute("id"),
+						channel: xOutput.getAttribute("channel"),
+					});
+			});
+			// add to render pass array
+			data.renderpass.push(pass);
+		});
+		// init shader, in not already
+		if (!this.shader) this.shader = new Shader(this.els.canvas[0], "auto", this.debug);
+		// return console.log(data);
+		this.shader.Load(data);
+		// reset & play if paused
+		if (this.shader.paused) this.shader.resetPlay();
+		// make sure window is focused
+		// if (!window.isFocues) window.focus();
 	}
 };
+
+const APP = yshader;
 
 window.exports = yshader;
---
+++ src/js/modules/test.js
@@ -0,0 +1,13 @@
+
+let Test = {
+	init(APP) {
+		// return;
+
+		// setTimeout(() => window.find(`.toolbar-tool_[data-arg="freecell"]`).trigger("click"), 100);
+
+		// temp
+		// window.maximize();
+		APP.start("Vortex");
+		// setTimeout(() => APP.els.canvas.trigger("click"), 5000);
+	}
+};
